4f674e086367ddaeb28512a52e941eb1fad74a42
Eduardo Zaghi <eduardozaghi@gmail.com>
2010-12-28 16:12:03 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simba::Application.config.secret_token = '1f5b3d4942c14947154f64dc2078af756ea1c9bb36fa45e3059fe401f37717baa62a603064952e835820d1736dc4f893ba4cd44cda73899ec032961c716f12b3'
